bc6f5378faaa0dcc679bbada246184d8e7470058
pandaemonium <pandae@pvpgn.org>
2006-02-27 20:56:31 +0000

    fix some ugly memory madness when a player disc'ed while being in-game

--- src/bnetd/ladder.cpp
+++ src/bnetd/ladder.cpp
@@ -198,7 +198,7 @@ extern int ladder_update(t_clienttag clienttag, t_ladder_id id, unsigned int cou
     if ((losers<1) || (winners<1) || (winners>1 && (winners!=losers)))
     {
 	eventlog(eventlog_level_info,__FUNCTION__,"missing winner or loser for count=%u (winners=%u losers=%u draws=%u) - discarding result",count,winners,losers,draws);
-	return 0;
+	return -1;
     }
 
     if (ladder_calc_info(clienttag,id,count,players,results,info)<0)
