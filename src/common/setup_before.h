397c6b286d2c20276584a09be19cea68fec90c1a
HarpyWar <harpywar@gmail.com>
2014-05-27 05:25:54 +0400

    Change version https://github.com/HarpyWar/pvpgn/issues/21

--- src/common/setup_before.h
+++ src/common/setup_before.h
@@ -126,7 +126,7 @@ const bool BNETD_CHANLOG = false;
 # define BNETD_DEFAULT_CONF_FILE "conf/bnetd.conf"
 #endif
 const char * const BNETD_FILE_DIR = "files";
-const char * const BNETD_SCRIPT_DIR = "script";
+const char * const BNETD_SCRIPT_DIR = "lua";
 const char * const BNETD_STORAGE_PATH = "";
 const char * const BNETD_REPORT_DIR = "reports";
 const char * const BNETD_LOG_FILE = "logs/bnetd.log";
