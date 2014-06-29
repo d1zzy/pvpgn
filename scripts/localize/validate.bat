b0cc40a0309aa1f14ba055c3481effbc16d403f4
HarpyWar <harpywar@gmail.com>
2014-06-29 16:31:15 +0400

    utility that validates XML translation files https://github.com/HarpyWar/pvpgn/issues/13

---
+++ scripts/localize/validate.bat
@@ -0,0 +1,15 @@
+@echo off
+
+pvpgn_localize_validator.exe ..\..\conf\i18n\common-deDE.xml.in
+pvpgn_localize_validator.exe ..\..\conf\i18n\common-csCZ.xml.in
+pvpgn_localize_validator.exe ..\..\conf\i18n\common-esES.xml.in
+pvpgn_localize_validator.exe ..\..\conf\i18n\common-frFR.xml.in
+pvpgn_localize_validator.exe ..\..\conf\i18n\common-itIT.xml.in
+pvpgn_localize_validator.exe ..\..\conf\i18n\common-jaJA.xml.in
+pvpgn_localize_validator.exe ..\..\conf\i18n\common-koKR.xml.in
+pvpgn_localize_validator.exe ..\..\conf\i18n\common-plPL.xml.in
+pvpgn_localize_validator.exe ..\..\conf\i18n\common-ruRU.xml.in
+pvpgn_localize_validator.exe ..\..\conf\i18n\common-zhCN.xml.in
+pvpgn_localize_validator.exe ..\..\conf\i18n\common-zhTW.xml.in
+
+pause
