e9d63f2678aaab3b8cef97a7f16ced3fb3183050
HarpyWar <harpywar@gmail.com>
2014-10-26 00:15:08 +0400

    generate a crash dump file on Windows https://github.com/HarpyWar/pvpgn/wiki/Debugging-Segment-Faults-(crash-dump)

--- src/win32/winmain.cpp
+++ src/win32/winmain.cpp
@@ -25,6 +25,9 @@
 
 #include <cstdio>
 #include <windows.h>
+#if DEBUG
+#include <dbghelp.h>
+#endif
 #include <windowsx.h>
 #include <winuser.h>
 #include <process.h>
@@ -824,11 +827,65 @@ namespace pvpgn
 
 }
 
+#if DEBUG
+void make_minidump(EXCEPTION_POINTERS* e)
+{
+	auto hDbgHelp = LoadLibraryA("dbghelp");
+	if (hDbgHelp == nullptr)
+		return;
+	auto pMiniDumpWriteDump = (decltype(&MiniDumpWriteDump))GetProcAddress(hDbgHelp, "MiniDumpWriteDump");
+	if (pMiniDumpWriteDump == nullptr)
+		return;
+
+	char name[MAX_PATH];
+	{
+		auto nameEnd = name + GetModuleFileNameA(GetModuleHandleA(0), name, MAX_PATH);
+		SYSTEMTIME t;
+		GetSystemTime(&t);
+		wsprintfA(nameEnd - strlen(".exe"),
+			"_%4d%02d%02d_%02d%02d%02d.dmp",
+			t.wYear, t.wMonth, t.wDay, t.wHour, t.wMinute, t.wSecond);
+	}
+
+	auto hFile = CreateFileA(name, GENERIC_WRITE, FILE_SHARE_READ, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
+	if (hFile == INVALID_HANDLE_VALUE)
+		return;
+
+	MINIDUMP_EXCEPTION_INFORMATION exceptionInfo;
+	exceptionInfo.ThreadId = GetCurrentThreadId();
+	exceptionInfo.ExceptionPointers = e;
+	exceptionInfo.ClientPointers = FALSE;
+
+	auto dumped = pMiniDumpWriteDump(
+		GetCurrentProcess(),
+		GetCurrentProcessId(),
+		hFile,
+		MINIDUMP_TYPE(MiniDumpWithIndirectlyReferencedMemory | MiniDumpScanMemory),
+		e ? &exceptionInfo : nullptr,
+		nullptr,
+		nullptr);
+
+	CloseHandle(hFile);
+
+	return;
+}
+
+LONG CALLBACK unhandled_handler(EXCEPTION_POINTERS* e)
+{
+	make_minidump(e);
+	return EXCEPTION_CONTINUE_SEARCH;
+}
+#endif
+
+
 using namespace pvpgn;
 using namespace pvpgn::bnetd;
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE reserved, LPSTR lpCmdLine, int nCmdShow)
 {
+#if DEBUG
+	SetUnhandledExceptionFilter(unhandled_handler);
+#endif
 	int result;
 	Console     console;
 
