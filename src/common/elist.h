c3994f023de3815b1ab875e96db904ac6613efab
Jacob Wiltse <jwiltse@control-tec.com>
2015-02-10 13:35:42 -0500

    Fixed formatting, really? we're using actual tab characters?

--- src/common/elist.h
+++ src/common/elist.h
@@ -80,8 +80,8 @@ namespace pvpgn
 			~iterator() throw() {}
 
 			iterator& operator=(const iterator& i) {
-                ptr = i.ptr;
-                return *this;
+				ptr = i.ptr;
+				return *this;
 			}
 
 			bool operator==(const iterator& op) const {
