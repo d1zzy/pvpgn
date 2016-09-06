62566ddec9017bc41175ff4d995d641176405adb
RElesgoe <RElesgoe@users.noreply.github.com>
2016-09-06 15:02:00 -0700

    Include common/setup_before.h before checking for HAVE_UNAME

--- src/compat/uname.cpp
+++ src/compat/uname.cpp
@@ -15,8 +15,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-#ifndef HAVE_UNAME
 #include "common/setup_before.h"
+#ifndef HAVE_UNAME
 #include "uname.h"
 
 #include <cerrno>
